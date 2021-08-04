b852c9c4bdcb26ed81c16015d27a026008daf143
martybmusic <scissorhandededward@gmail.com>
2021-08-04 14:33:18 -0500

    Display Component passing test / MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,75 @@
+import React from 'react';
+import { getByText, findAllByTestId, findByTestId, findByText, getByRole, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import Display from '../Display';
+import mockFetchShow from '../../api/fetchShow';
 
+jest.mock("../../api/fetchShow")
 
+const testShow = {
+    name: "Test: The Show",
+    image: {
+        medium: "https://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg",
+        original: "https://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg"
+    },
+    summary: "summary test",
+    seasons: [{
+        id: 0,
+        name: 'Alf',
+        episodes: []
+    },
+    {
+        id: 1,
+        name: 'Elf',
+        episodes: []
+    },
+    {
+        id: 2,
+        name: 'Ulf',
+        episodes: []
+    }
+    ]
+};
 
+test("Display component renders without passed in props", () => {
+    render(<Display/>);
+    const imageSelector = screen.queryByAltText("header image");
+    expect(imageSelector).toBeInTheDocument();
+});
 
+test("Show component displays correct # of seasons when fetched", async () => {
+    render(<Display/>);
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    
+    const button = screen.getByRole("button");
+    userEvent.click(button);
 
+    await waitFor(() => {
+        const showContainer = screen.getByTestId("show-container");
+        expect(showContainer).toBeInTheDocument();
+    });
 
+    const seasonOptions = screen.getAllByTestId("season-option");
+    expect(seasonOptions).toHaveLength(3);
+});
 
+test("Button click calls displayFunction", async () => {
+    mockFetchShow.mockResolvedValue(testShow);
+    const mockDisplayFunc = jest.fn();
 
+    render(<Display displayFunc={mockDisplayFunc}/>)
 
+    const button = screen.getByRole("button");
+    userEvent.click(button);
 
-
-
-
+    await waitFor(() => {
+        expect(mockDisplayFunc.mock.calls.length).toBe(1);
+        expect(mockDisplayFunc.mock.calls).toHaveLength(1);
+        expect(mockDisplayFunc.mock.calls.length === 1).toBeTruthy();
+        expect(mockDisplayFunc).toHaveBeenCalled();
+    })
+})
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
